508fe36145645bff68ee94abcc9ed2abd169806e
Nikolas Clay <lydiaandnikolas@gmail.com>
2017-11-09 11:36:47 -0600

    pulling changes

--- SCMS.UI/Scripts/scms.js
+++ SCMS.UI/Scripts/scms.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+	function link();
+});
+
+$(function link() {
+	$('#urlddl').change(function () {
+		var url = $(this).val();
+		if (url != null && url != '') {
+			window.location.href = url;
+		}
+	});
+});
